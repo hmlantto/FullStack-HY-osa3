b6636c1a2b78c5d3058e776ea0b495c3bf3b7b36
Helka Lantto <hmlantto@gmail.com>
2021-04-20 16:01:43 +0300

    Commit up to exercise 3.6

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require( 'express' ) ;
-const app = express() ;
+const { response, request } = require('express')
+const express = require( 'express' )
+const app = express()
+
+app.use( express.json() )
 
 let persons = [
   {
@@ -22,13 +25,79 @@ let persons = [
     name: "Mary Poppendieck",
     number: "39-23-6423122"
   }
-];
+]
 
-app.get( '/api/persons', ( req, res ) => {
-  res.json( persons ) ;
-});
+const generateId = () => {
+  const existingIds = persons.map( n => n.id )
+  let newId
 
-const port = 3001 ;
-app.listen( port, () => {
+  do {
+    newId = Math.floor( Math.random() * 100 + 1 )
+  } while ( existingIds.find( x => x === newId ) )
+
+  return newId
+}
+
+app.get( '/info', ( request, response ) => {
+  response.send( `<p>Phonebook has info for ${persons.length} people</p>
+              <p>${new Date()}</p>` )
+})
+
+app.get( '/api/persons', ( request, response ) => {
+  response.json( persons )
+})
+
+app.get( '/api/persons/:id', ( request, response ) => {
+  const id = Number( request.params.id )
+  const person = persons.find( person => person.id === id )
   
+  if ( person ) {
+    response.json( person )
+  } else {
+    response.status( 404 ).end()
+  }
+})
+
+app.delete( '/api/persons/:id', ( request, response ) => {
+  const id = Number( request.params.id )
+  persons = persons.filter( person => person.id !== id )
+
+  response.status( 204 ).end()
+})
+
+app.post( '/api/persons', ( request, response ) => {
+  const body = request.body
+
+  if ( !body.name ) {
+    return response.status( 400 ).json({
+      error: 'name required'
+    })
+  }
+
+  if ( !body.number ) {
+    return response.status( 400 ).json({
+      error: 'number required'
+    })
+  }
+
+  if ( persons.find( x => x.name === body.name ) ) {
+    return response.status( 400 ).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat( person )
+
+  response.json( person )
+})
+
+const port = 3001
+app.listen( port, () => {
+
 })
