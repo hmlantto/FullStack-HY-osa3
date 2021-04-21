a8d2450f9187ef22f69ceebc3c11afcf7a11c830
Helka Lantto <hmlantto@gmail.com>
2021-04-21 12:02:58 +0300

    Add frontend

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express()
 app.use( cors() )
 app.use( morgan( ':method :url :status :res[content-length] - :response-time ms :body' ) )
 app.use( express.json() )
+app.use( express.static( 'build' ) )
 
 let persons = [
   {
